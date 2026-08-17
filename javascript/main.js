3407db3ba448987255bd8ef750eecc6ec1c3d9ed
Tolya <tolya@webrazrabotka.ru>
2026-08-17 13:38:04 +0300

    Added footer

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,62 @@
-$(document).ready(function(){
-$('.sliderTreeOsTrack').slick({
-  		autoplay:false,
-		autoplaySpeed:3500,
-		slidesToShow:1,
-		slidesToScroll:1,
-		dots:true,
-		swipeToSlide: true,
-		variableWidth: true,
-		arrows:false,
-		infinite:true,
-		speed:750,
-		fade:true,
-		easing:'easeInOutQuart',
-		slide:'.slideTOT'
-  });
+$(document).ready(function() {
+    $('.sliderTreeOsTrack').slick({
+        autoplay: false,
+        autoplaySpeed: 3500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        swipeToSlide: true,
+        variableWidth: true,
+        arrows: false,
+        infinite: true,
+        speed: 750,
+        fade: true,
+        easing: 'easeInOutQuart',
+        slide: '.slideTOT'
+    });
+
+    // Яндекс Карта 
+
+    var $maps = $("#map"); 
+
+    if ($maps.length > 0) {
+        var center = [56.32257053322084, 44.00141489727857];
+
+        function createMap(mapId) {
+            var map = new ymaps.Map(mapId, {
+                center: center,
+                zoom: 12,
+            });
+
+            var placemark = new ymaps.Placemark(
+                center,
+                {},
+                {
+                    iconLayout: "default#image",
+                    iconImageHref: "/images/svg/marker.svg",
+                    iconImageSize: [40, 40],
+                    iconImageOffset: [-19, -44],
+                }
+            );
+
+            placemark.events.add("click", function () {
+                var url = "https://yandex.ru/maps/-/CTgRBMj-";
+                window.open(url, "_blank");
+            });
+
+            map.controls.remove("geolocationControl");
+            map.controls.remove("searchControl");
+            map.controls.remove("trafficControl");
+            map.controls.remove("typeSelector");
+            map.controls.remove("fullscreenControl");
+            map.controls.remove("zoomControl");
+            map.controls.remove("rulerControl");
+
+            map.geoObjects.add(placemark);
+        }
+
+        ymaps.ready(function() {
+            createMap("map");
+        });
+    }
 });
